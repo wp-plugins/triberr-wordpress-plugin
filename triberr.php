c508443c0fe8ffe7fbb2ebb40b29c6f0554e4de4
TriberrGlobal <TriberrGlobal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-03 20:34:20 +0000

    Minor update: add Triberr ID to posts imported by the plugin
    git-svn-id: https://plugins.svn.wordpress.org/triberr-wordpress-plugin/tags/2.0.2@514037 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- triberr.php
+++ triberr.php
@@ -1,20 +1,21 @@
 <?php
-/*
+/* 
 Plugin Name: Triberr
 Plugin URI: http://triberr.com/subdomains/plugins/wordpress/
 Description: Instantly send posts from your blog from Triberr.
-Version: 1.0.0
+Version: 2.0.2
 Author: Triberr
 Author URI: http://Triberr.com/
 License: GPL2
 */
-function triberr_grab_base_url() {
-	$siteurl = get_option('siteurl');
-	$baseurl = $siteurl . '/wp-content/plugins/' . basename(dirname(__FILE__));
-	echo $baseurl;
-}
 
+$version_number = "2.0.2";
 
+// Include calls for xml-rpc
+require_once('triberr_includes/class-triberr-xmlrpc-server.php');
+
+// Include stylesheets and javascripts
+add_action('admin_head', 'triberr_admin_register_head');
 function triberr_admin_register_head() {
 	$siteurl = get_option('siteurl');
     $styleurl = $siteurl . '/wp-content/plugins/' . basename(dirname(__FILE__)) . '/media/styles/manage.css';
@@ -23,16 +24,18 @@ function triberr_admin_register_head() {
     echo "<script type='text/javascript' src='$jsurl'></script>\n";
 }
 
+// Create the sidebar link
+add_action('admin_menu', 'triberr_menu'); 
 function triberr_menu() {
 	add_options_page('Triberr Options', 'Triberr', 'manage_options', 'triberr-options', 'triberr_options_page');
+	add_option( 'triberr_comments_width', '600', '', 'yes' );
+	add_option( 'triberr_comments_bg_color', 'ffffff', '', 'yes' );
+	add_option( 'triberr_comments_bg_color', 'ffffff', '', 'yes' );
+	add_option( 'triberr_comments_status', 'on', '', 'yes' );
 }
 
 
-add_action('admin_head', 'triberr_admin_register_head');
-
-add_action('admin_menu', 'triberr_menu'); 
-
-
+// Check if the user has permissions to edit the options page
 function triberr_options_page() {
 	if (!current_user_can('manage_options'))  {
 		wp_die( __('You do not have sufficient permissions to access this page.') );
@@ -86,17 +89,60 @@ function triberr_options_page() {
         </div>
 	</div>
     
-    <!--
+    <?php if($PINGKEY != ""){ ?>
     <div id="authorize" class="postbox" style="float:left;width:63%;">
-    <h3 >Frictionless Guest Posting</h3>
+    <h3 >ReBlog Settings</h3>
     	<div class="inside">
         
-        <iframe src="http://triberr.com/subdomains/plugins/fgp-authorize.php?key=<?php echo $PINGKEY; ?>&domain=<?PHP echo grab_base_url(); ?>" width="600" height="200"></iframe>
+        <iframe src="http://triberr.com/subdomains/plugins/fgp-authorize.php?key=<?php echo $PINGKEY; ?>&domain=<?PHP echo get_option('siteurl'); ?>" width="600" height="160"></iframe>
         
        
         </div>
 	</div>
-    -->
+    
+    
+    <div id="comment" class="postbox" style="float:left;width:63%;">
+    <h3 >Triberr Comments</h3>
+    	<div class="inside">
+        
+        <form method="post" action="options.php" class="form-table">
+        <?php wp_nonce_field('update-options');
+            $triberr_comments_bg_color = get_option('triberr_comments_bg_color');
+            $triberr_comments_status = get_option('triberr_comments_status');
+            $triberr_comments_width = get_option('triberr_comments_width');
+			?>
+        <ul>
+            	<li>
+                   Enable Triberr's commenting system?<br />
+                   <small>Any comments from Triberr or any reblogged versions of this post will also be included</small> <br />
+                   <select name="triberr_comments_status" id="triberr_comments_status">
+                   	<option value="yes" <?php if($triberr_comments_status == "yes"){ echo "selected=\"selected\""; } ?>>Yes</option>
+                    <option value="no" <?php if($triberr_comments_status == "no"){ echo "selected=\"selected\""; } ?>>No</option>
+                   </select>
+                 </li>  
+                   <br />
+                <li>
+                   Look and feel<br />
+                   <small>This will control the look of the comments on your site</small> <br />
+                    Background color: #<input name="triberr_comments_bg_color" id="triberr_comments_bg_color" type="text" value="<?php echo $triberr_comments_bg_color; ?>" style="width: 70px;"/><br />
+                    Width: <input name="triberr_comments_width" id="triberr_comments_width" type="text" value="<?php echo $triberr_comments_width; ?>" style="width: 50px;"/>px<br />
+                   
+                 </li>     
+                <input type="hidden" name="action" value="update" />
+                <input type="hidden" name="action" value="update" />
+                <input type="hidden" name="page_options" value="triberr_comments_bg_color,triberr_comments_width,triberr_comments_status" />
+                <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+              
+            </ul>
+        </form> 
+       
+        </div>
+	</div>
+   
+   <?PHP } ?> 
+    
+    
+    
 </div>
 
 
@@ -106,15 +152,71 @@ function triberr_options_page() {
 }
 
 
-
+// Listen for RPC's
 add_action('admin_footer', 'triberr_enable_remote'); 
+function triberr_enable_remote (){
+	$option_name = "enable_xmlrpc";
+	$newvalue = "1";
+	update_option($option_name, $newvalue);
+}
+
 add_action ('publish_post', 'triberr_submit_post');
+add_action ('Triberr.getSourceID', 'triberr_update_meta');
 add_action ('admin_footer', 'triberr_display_message');
 
+// Write the comment plugin to our code
+class triberr_append_post {
+		
+	function triberr_append_post_function( $content )
+	{	
+		global $post;
+		if( triberr_append_post::triberr_is_post())
+		{
+			//global $post;
+			$triberr_id = get_post_meta($post->ID, '_triberr_id', true);
+			$url = get_permalink( $post->ID );
+			$triberr_comments_bg_color = get_option('triberr_comments_bg_color');
+            $triberr_comments_status = get_option('triberr_comments_status');
+            $triberr_comments_width = get_option('triberr_comments_width');
+			
+		$plugin="
+        <script type=\"text/javascript\">
+  function resizeCrossDomainIframe(id, other_domain) {
+    var iframe = document.getElementById(id);
+    window.addEventListener('message', function(event) {
+      if (event.origin !== other_domain) return; // only accept messages from the specified domain
+      if (isNaN(event.data)) return; // only accept something which can be parsed as a number
+      var height = parseInt(event.data) + 32; // add some extra height to avoid scrollbar
+      iframe.height = height + \"px\";
+    }, false);
+  }
+</script>
+<div id=\"triberr_comment_system\" style=\"margin: 10px 0 0 0; float: left;\">
+<iframe src='http://triberr.com/subdomains/plugins/comments/triberr_comment_loader.php?post_id=$triberr_id&bgcolor=$triberr_comments_bg_color&width=$triberr_comments_width&parent=$url' scrolling=\"no\" frameborder=\"0\" width=\"$triberr_comments_width\" id=\"my_iframe\" onload=\"resizeCrossDomainIframe('my_iframe', 'http://triberr.com');\">
+</iframe>
+</div>
+        ";
+			if($triberr_comments_status == "yes" && $triberr_id != ""){ 
+			$content = $content .  $plugin;
+			}
+		} 
+		return $content;
+		
+	}	
+	function triberr_is_post()
+	{
+		global $post;
+		return ( 'post' == $post->post_type );
+	}
+}
+
+// Hook into the post when loaded
+add_filter( 'the_content', array( 'triberr_append_post', 'triberr_append_post_function' ));
 
 
 
 
+/*
 function triberr_multiple($posts = NULL) {
 	if($posts != NULL) {
 		foreach($posts as $post) {
@@ -135,6 +237,7 @@ function triberr_multiple($posts = NULL) {
 	}
 }
 
+
 function triberr_categories($PINGKEY = NULL) {
 	$triberrCATS = triberr_connect('http://triberr.com/triber/subdomains/api/?key='.$PINGKEY.'&act=tribes');
 
@@ -149,24 +252,32 @@ function triberr_categories($PINGKEY = NULL) {
 		}
 	}
 }
+*/
+
 
 //ACTUALLY PING THE PUBLISHED POST TO triberr
 function triberr_build_url($post_ID) {
 	$thisPost = get_post($post_ID, ARRAY_A);
 	
+	$triberr_id = get_post_meta($post_ID, '_triberr_id', true);
+	if($triberr_id == ""){
+		$triberr_id = "empty";
+	} 
+	
 	$fields = array(
 		'act'=>urlencode("add"),
 		'key'=>urlencode(get_option('triberr_apikey')),
-		'url'=>urlencode(get_permalink( $post_ID )),
+		'url'=>urlencode(get_permalink(  $thisPost['ID'] )),
 		'title'=>urlencode($thisPost['post_title']),
-		'body'=>urlencode(apply_filters('the_content',$thisPost['post_content'])),  //apply_filters('the_content', $post->post_content)
+		'body'=>urlencode(apply_filters('the_content',$thisPost['post_content'])), 
 		'status'=>urlencode($thisPost['post_status']),
 		'guid'=>urlencode($thisPost['guid']),
-		'post_date'=>urlencode($thisPost['post_date']),
-		'post_id'=>urlencode($thisPost['ID']),
+		'post_date'=>urlencode($thisPost['post_date']), 
+		'post_id'=>urlencode( $thisPost['ID']),
 		'post_type'=>urlencode($thisPost['post_type']),
-	);
-	//echo "body: " . urlencode(apply_filters('the_content',$thisPost['post_content']));
+		'triberr_id'=>$triberr_id,
+		'plugin_version'=>urlencode($version_number),
+		);
 	return $fields;
 }
 
@@ -203,7 +314,7 @@ function triberr_submit_post($post_ID) {
 	$thisPost = get_post($post_ID, ARRAY_A);
 			if(get_permalink( $post_ID ) != NULL) {
 				$triberrFields = triberr_build_url($post_ID);
-				$url = 'http://triberr.com/triber/subdomains/api/';
+				$url = 'http://triberr.com/subdomains/api/';
 				$triberrMSG = triberr_connect($url, $triberrFields);
 			}
 
@@ -215,10 +326,5 @@ function triberr_display_message() {
 		echo '<div id="message" class="updated"><p>'.get_option('triberr_message').'</p></div>';
 		update_option('triberr_message', '');
 	}
-}
-function triberr_enable_remote (){
-	$option_name = "enable_xmlrpc";
-	$newvalue = "1";
-	update_option($option_name, $newvalue);
 }
 ?>
