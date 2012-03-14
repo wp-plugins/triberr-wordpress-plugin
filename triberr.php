06ac4276d5be997c1ac8fd6951fe5e2b0b0da58e
TriberrGlobal <TriberrGlobal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-14 16:26:35 +0000

    tagging version 2.0.3
    git-svn-id: https://plugins.svn.wordpress.org/triberr-wordpress-plugin/tags/2.0.3@518956 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- triberr.php
+++ triberr.php
@@ -1,224 +1,70 @@
-<?php
-/*
-Plugin Name: Triberr
-Plugin URI: http://triberr.com/subdomains/plugins/wordpress/
-Description: Instantly send posts from your blog from Triberr.
-Version: 1.0.0
-Author: Triberr
-Author URI: http://Triberr.com/
-License: GPL2
-*/
-function triberr_grab_base_url() {
-	$siteurl = get_option('siteurl');
-	$baseurl = $siteurl . '/wp-content/plugins/' . basename(dirname(__FILE__));
-	echo $baseurl;
-}
+=== Plugin Name ===
+Contributors: Dan Cristo, Andres Hernandez, Dino Dogan
+Tags: triberr, twitter, remote publishing, blogging, guest post, guest posting, blog syndication, comment system, reBlog, social media, guest post
+Requires at least: 2.0.2
+Tested up to: 3.1.2
+Stable tag: 2.0.3
 
+What does Triberr Plugin do?
 
-function triberr_admin_register_head() {
-	$siteurl = get_option('siteurl');
-    $styleurl = $siteurl . '/wp-content/plugins/' . basename(dirname(__FILE__)) . '/media/styles/manage.css';
-    $jsurl = $siteurl . '/wp-content/plugins/' . basename(dirname(__FILE__)) . '/scripts/triberr_js.js';
-    echo "<link rel='stylesheet' type='text/css' href='$styleurl' />\n";
-    echo "<script type='text/javascript' src='$jsurl'></script>\n";
-}
+Sends your post immediately to Triberr without needing to crawl your RSS feed. 
+Allows tribemates to view your post in their Tribal Stream thereby saving them time and increasing the likelihood of them sharing your post.
+Allows you to send and receive guest posts to your blog through Triberr (reBlog/syndication function)
+Pulls Bio/pic/social information from your Triberr profile and ensures every reBlog has proper attribution. 
+Installs a unified view of all comments made across every instance of syndicated content. 
 
-function triberr_menu() {
-	add_options_page('Triberr Options', 'Triberr', 'manage_options', 'triberr-options', 'triberr_options_page');
-}
+== Description ==
 
+Triber
+This is an all-in-one plugin for members of Triberr.com. This is meant to seamlessly integrate your blog with Triberr so that it can send and receive posts, comments and guest posts.
+**More info:**
 
-add_action('admin_head', 'triberr_admin_register_head');
+* [Triberr](http://triberr.com/blog.php?post=24560)
 
-add_action('admin_menu', 'triberr_menu'); 
+== Installation ==
 
+1. Upload the contents of triberr.zip to your plugins directory.
+2. Activate the plugin
+3. There will be a "Triberr" menu on your admin panel.
+4. Login to Triberr.com and go to your Account Settings -> Blog Settings page (http://triberr.com/settings/?pg=rss). Click on "Show API Token". Copy the 35 digit string of characters and numbers.
+5. Paste the API Token you copied from your Triberr Blog Settings page and paste it in the plugins page within Wordpress admin. Save.
+4. That's all. When you publish or update a post, it will automatically be sent to Triberr. You can pause or delete any unwanted posts by logging into Triberr and going to your Sent Stream.
 
-function triberr_options_page() {
-	if (!current_user_can('manage_options'))  {
-		wp_die( __('You do not have sufficient permissions to access this page.') );
-	}
+== Frequently Asked Questions ==
 
-?>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" type="text/javascript"></script>
-<div class="wrap">
-<?php // echo triberr_connect('http://triberr.com/triber/subdomains/api/announce.php'); ?>
-    <div class="top_nav">
-        <div class="plugin_logo">
-            <img src="http://triberr.com/assets/misc/logoLight.png" />
-        </div>
-	</div>
-    
-    <h2>Triberr Plugin Settings</h2>
-    <div id="api" class="postbox" style="float:left;width:63%;">
-    <h3 >API Blog Token</h3>
-    	<div class="inside">
-        <form method="post" action="options.php" class="form-table">
-            <?php wp_nonce_field('update-options');
-            $PINGKEY = get_option('triberr_apikey');
-            ?>
-            <ul>
-            	<li>
-                    Your API blog token connects this blog with your Triberr profile. <br />
-                    You can find your token in your Triberr Account Settings on the Blog Settings page. <br /><br />
-                    <small>Hint: Looks like a 35 character random string of numbers and letters</small> <br />
-                    <input id="triberr_apikey" maxlength="35" size="35" name="triberr_apikey" value="<?php echo $PINGKEY; ?>" /> 
-                    <small> <a href="#" onclick="toggle_visibility('api_help');">Help me find it</a></small></li>
-            	</li>
-                <input type="hidden" name="action" value="update" />
-                <input type="hidden" name="page_options" value="triberr_apikey" />
-                <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
-                <br />
-                <small>After saving, all future posts will automatically be sent to Triberr.</small>
-            </ul>
-        </form> 
-       		 <div id="api_help" style="display: none;">
-                 <hr />
-            	<h4>Help Finding your API Blog Token</h4>
-                If you are logged into Triberr.com <a href="http://triberr.com/settings/?pg=rss">Click Here</a> for your API token. <br />
-                Your API Blog Token is a 35 character code acts as a unique identifier between Triberr and your blog. <br />
-                <strong>Example:</strong> f31sP9di3z3jd93o93wM9dj390jfi2Lph39 <br /><br />
-                
-                Your API Blog Token can be found by logging into Triberr. Navigate to your "Global Account Settings" then your "Blog Settings". 
-                Each blog will have a unique Blog API Token.			
-                <h4>Video Guide</h4>
-                <iframe width="420" height="315" src="http://www.youtube.com/embed/g3ORoLXjhnI" frameborder="0" allowfullscreen></iframe>
-            </div>  
-        </div>
-	</div>
-    
-    <!--
-    <div id="authorize" class="postbox" style="float:left;width:63%;">
-    <h3 >Frictionless Guest Posting</h3>
-    	<div class="inside">
-        
-        <iframe src="http://triberr.com/subdomains/plugins/fgp-authorize.php?key=<?php echo $PINGKEY; ?>&domain=<?PHP echo grab_base_url(); ?>" width="600" height="200"></iframe>
-        
-       
-        </div>
-	</div>
-    -->
-</div>
+= Will I be able to select certain categories to send to Triberr?
 
+Right now all posts go to Triberr, but you can delete them from you Sent Stream. Future versions will give you control over which posts you send.
 
+= If I make a change to a post, will Triberr import it again?
 
-<?php
-	//FORM CODE ENDS HERE
-}
+Triberr will check to see if we already know about the post in our database. If so, we will not import it again. 
 
+= Can I also import older posts into Triberr using this plugin?
 
+No, Triberr will only accept posts published within the last week.
 
-add_action('admin_footer', 'triberr_enable_remote'); 
-add_action ('publish_post', 'triberr_submit_post');
-add_action ('admin_footer', 'triberr_display_message');
+= If my post gets syndicated to other blogs, will I get the credit.
 
+Yes. Every reBlog results in the Author info pulled along with the content, so regardless of where your post is published you will be prominently featured as the Author. 
 
+== Screenshots ==
 
+1. Screenshots and an installation guide can be found at: http://triberr.com/blog.php?post=24560
 
+== Changelog ==
+= 2.0.3 =
+Saving the plugin version number
+Fixed an issue with the default setting for global comment system from "Yes" to "On"
 
-function triberr_multiple($posts = NULL) {
-	if($posts != NULL) {
-		foreach($posts as $post) {
-			$triberrURL = triberr_build_url($post);
-			$triberrMSG = triberr_connect($triberrURL);
-			echo "<p>".$triberrMSG."</p>";
-		}
-	} else {
-		global $post;
-		$thisCount = 1;
-		$myposts = get_posts('numberposts=-1');
-		foreach($myposts as $post) :
-			setup_postdata($post);
-			$triberrURL = triberr_build_url($post->ID);
-			$triberrMSG = triberr_connect($triberrURL);
-			echo "<p>".$triberrMSG."</p>";
-		endforeach;
-	}
-}
+= 2.0.2 =
+Saved the Triberr ID to the Wordpress Meta Data
 
-function triberr_categories($PINGKEY = NULL) {
-	$triberrCATS = triberr_connect('http://triberr.com/triber/subdomains/api/?key='.$PINGKEY.'&act=tribes');
+= 2.0.1 =
+Fixed a function conflict that crashed the plugin
 
-	preg_match_all('#<id>(.*?)</id>#is', $triberrCATS, $PINFO[id], PREG_SET_ORDER);
-	preg_match_all('#<name>(.*?)</name>#is', $triberrCATS, $PINFO[name], PREG_SET_ORDER);
+= 2.0 =
+Triberr Global Comment System
 
-	foreach($PINFO[id] as $key => $value) {
-		if(get_option('triberr_category') === $PINFO[id][$key][1]) {
-			echo '<option value="'.$PINFO[id][$key][1].'" selected=selected>'.$PINFO[name][$key][1].'</option>';
-		} else {
-			echo '<option value="'.$PINFO[id][$key][1].'">'.$PINFO[name][$key][1].'</option>';
-		}
-	}
-}
-
-//ACTUALLY PING THE PUBLISHED POST TO triberr
-function triberr_build_url($post_ID) {
-	$thisPost = get_post($post_ID, ARRAY_A);
-	
-	$fields = array(
-		'act'=>urlencode("add"),
-		'key'=>urlencode(get_option('triberr_apikey')),
-		'url'=>urlencode(get_permalink( $post_ID )),
-		'title'=>urlencode($thisPost['post_title']),
-		'body'=>urlencode(apply_filters('the_content',$thisPost['post_content'])),  //apply_filters('the_content', $post->post_content)
-		'status'=>urlencode($thisPost['post_status']),
-		'guid'=>urlencode($thisPost['guid']),
-		'post_date'=>urlencode($thisPost['post_date']),
-		'post_id'=>urlencode($thisPost['ID']),
-		'post_type'=>urlencode($thisPost['post_type']),
-	);
-	//echo "body: " . urlencode(apply_filters('the_content',$thisPost['post_content']));
-	return $fields;
-}
-
-//SETTING UP THE OPTIONS PAGE
-function triberr_connect($url, $fields) {
-	//if (function_exists('curl_init')) {
-		
-		//url-ify the data for the POST
-		foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-		rtrim($fields_string,'&');		
-		
-		//open connection
-		$ch = curl_init();
-		
-		//set the url, number of POST vars, POST data
-		curl_setopt($ch,CURLOPT_URL,$url);
-		curl_setopt($ch,CURLOPT_POST,count($fields));
-		curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
-		curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-		
-		//execute post
-		$result = curl_exec($ch);
-		
-		//close connection
-		curl_close($ch);
-		//print $result;		 
-		return $result;
-//	} else {
-	//	return file_get_contents($url);
-	//}
-}
-
-function triberr_submit_post($post_ID) {
-	$thisPost = get_post($post_ID, ARRAY_A);
-			if(get_permalink( $post_ID ) != NULL) {
-				$triberrFields = triberr_build_url($post_ID);
-				$url = 'http://triberr.com/triber/subdomains/api/';
-				$triberrMSG = triberr_connect($url, $triberrFields);
-			}
-
-	update_option('triberr_message', $triberrMSG);
-}
-
-function triberr_display_message() {
-	if(get_option('triberr_message')) {
-		echo '<div id="message" class="updated"><p>'.get_option('triberr_message').'</p></div>';
-		update_option('triberr_message', '');
-	}
-}
-function triberr_enable_remote (){
-	$option_name = "enable_xmlrpc";
-	$newvalue = "1";
-	update_option($option_name, $newvalue);
-}
-?>
+= 1.0 =
+Beta release
